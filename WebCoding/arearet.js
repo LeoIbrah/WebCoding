0b41987dca0af55ad7e88e51e9f941fc2f048fb7
LeoIbrah <leonardo.souzaa372@gmail.com>
2026-09-21 11:05:43 -0300

    Add files via upload

---
+++ WebCoding/arearet.js
@@ -0,0 +1,3 @@
+// Área do Retângulo
+
+let comprimento = parseFloat(prompt("Digite o comprimento do retângulo"))
---
+++ WebCoding/areatr.js
@@ -0,0 +1,8 @@
+// Área do Triângulo
+
+let base = parseFloat(prompt("Digite a base do triângulo"));
+let area = parseFloat(prompt("Digite a altura do triângulo"));
+
+let area = (base * altura) / 2;
+
+console.log("Área do triângulo: " + area.toFixed(2));
